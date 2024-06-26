1ec6fcca77cccbc2f7b3c80675cd08e283125ee2
Aiden Janey <arjaney.professional@gmail.com>
2024-06-26 09:45:02 -0400

    Using Docker to make app deployable

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost:27017/PasswordDB', (err)=>{
-    if(!err)
-        console.log('MongoDB Connection Successful!');
-    else
-        console.log('Error in Database Connection: ' + JSON.stringify(err, undefined, 2));
+mongoose.connect("mongodb://mongo-db:27017/PasswordDB", (err) => {
+  if (!err) console.log("MongoDB Connection Successful!");
+  else
+    console.log(
+      "Error in Database Connection: " + JSON.stringify(err, undefined, 2)
+    );
 });
 
 module.exports = mongoose;
